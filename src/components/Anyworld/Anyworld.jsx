eef131c0eab55d66886488a8e21565b4ecf5d1bc
Youseef EMAM <152444249+Youseef-Emam@users.noreply.github.com>
2024-02-05 14:08:49 +0200

    Add files via upload

---
+++ src/components/Anyworld/Anyworld.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Anyworld extends Component {
+  render() {
+    return <>
+    
+    <h1 className='text-center min-vh-100'>Sorry Page Not Found Use Navbar To Move :)</h1>
+    
+    
+    </>
+  }
+}
